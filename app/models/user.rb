71ce91a02ac4ca61b57437cbae3fd34d218dfb33
AhmadiGu <ahmadigulalai76@gmail.com>
2023-04-24 16:02:19 +0500

    Linters errors fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-    has_many :reservations
-    has_many :cars, through: :reservations
+  has_many :reservations
+  has_many :cars, through: :reservations
 
-    validates :user_name, presence: true
-    validates :email, presence: true, uniqueness: true
-    validates :password, length: { minimum: 6 }
+  validates :user_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6 }
 end
