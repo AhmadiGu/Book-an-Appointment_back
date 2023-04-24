cb77569651ea6532c07fb6be1cd9951dc10134f4
SekoViper <damehlivingstone1@gmail.com>
2023-04-24 14:22:17 +0000

    Add cars controller.

---
+++ app/controllers/api/v1/cars_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::CarsController < ApplicationController
+  def show
+    @car = Car.find(params[:id])
+    render json: @car
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:name, :description, :photo, :price_per_day, :car_model, :duration)
+  end
+end
