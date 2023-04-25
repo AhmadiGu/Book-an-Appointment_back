165dfa3a9c43dc435671791b97051b0b88397125
SekoViper <damehlivingstone1@gmail.com>
2023-04-25 13:22:45 +0000

    Fix linter error.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 class ApplicationController < ActionController::API
-    # Devise code
+  # Devise code
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   # Doorkeeper code
   before_action :doorkeeper_authorize!
   respond_to :json
 
   protected
-  
+
   # Devise methods
   # Authentication key(:username) and password field will be added automatically by devise.
   def configure_permitted_parameters
-    added_attrs = [:email, :first_name, :last_name]
+    added_attrs = %i[email first_name last_name]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
