9c700c00a2744fd39f6b97277e37ac0310d678b9
SekoViper <damehlivingstone1@gmail.com>
2023-04-25 10:50:29 +0000

    Modify file.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,18 @@
 class User < ApplicationRecord
-  has_many :reservations
-  has_many :cars, through: :reservations
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
-  validates :user_name, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :password, length: { minimum: 6 }
+  validates :email, format: URI::MailTo::EMAIL_REGEXP
+  enum role: %i[user admin]
+  def self.authenticate(email, password)
+    user = User.find_for_authentication(email: email)
+    user&.valid_password?(password) ? user : nil
+  end
+  validates :username, presence: true
+
+  validates :username, length: { minimum: 3, maximum: 70 }
+
+  has_many :reservations
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 # create users
-user1 = User.create(user_name: "John", email: "john@example.com", password: "password123")
-user2 = User.create(user_name: "Jane", email: "jane@example.com", password: "password123")
+user1 = User.create(username: "John", email: "john@example.com", password: "$2a$12$457wUfgvrN1HeXZjQRT4pes5mEIZAAhrBP9PuEm9pjieD8aQnpIWq")
+# user2 = User.create(username: "Jane", email: "jane@example.com")
 # create cars
 car1 = Car.create(name: "Toyota Camry", photo: " png" , description: "Sedan car", price_per_day: 150, car_model: "Camry 2022", user_id: user1.id)
 car2 = Car.create(name: "Honda Civic", photo: " png" , description: "Compact car", price_per_day: 120, car_model: "Civic 2022", user_id: user1.id)
