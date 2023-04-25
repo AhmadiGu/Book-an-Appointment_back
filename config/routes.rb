a9eb643b1e669d5ecabf83c5dc1107125c0c4c09
omarbabou <obabou50@gmail.com>
2023-04-25 10:24:05 +0200

    Add destroy to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :cars, only: [:index,:show]
+      resources :cars, only: [:index,:show,:destroy]
     end
   end
 end
