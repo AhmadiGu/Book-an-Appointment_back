53b2bad9642237802d6bdffad46c1b2753e1f769
SekoViper <damehlivingstone1@gmail.com>
2023-04-27 14:20:06 +0000

    Refactor routes

--- config/routes/api.rb
+++ config/routes/api.rb
@@ -1,23 +1,24 @@
 namespace :api do
-    namespace :v1 do
-      scope :users, module: :users do
-        post '/', to: 'registrations#create', as: :user_registration
-      end
-      get '/users/me', to: 'users#me'
-      get '/cars', to: 'cars#index', as: :cars
-      post 'cars', to: 'cars#create'
-      delete 'cars', to: 'cars#destroy'
-      get 'cars/(:id)', to: 'cars#show'
-      get '/reservations', to: 'reservations#index'
-      post 'reservations/(:id)', to: 'reservations#show'
-      post 'cars/(:id)/reservations', to: 'reservations#create'
+  namespace :v1 do
+    scope :users, module: :users do
+      post '/', to: 'registrations#create', as: :user_registration
     end
-  end
 
-  scope :api do
-    scope :v1 do
-      use_doorkeeper do
-        skip_controllers :authorizations, :applications, :authorized_applications
-      end
+    get '/users/me', to: 'users#me'
+
+    resources :cars, only: [:index, :create, :destroy, :show]
+
+    resources :reservations, only: [:index, :create, :show]
+    post 'cars/:id/reservations', to: 'reservations#create'
+
+    post '/oauth/token/info', to: 'oauth#token_info'
+  end
+end
+
+scope :api do
+  scope :v1 do
+    use_doorkeeper do
+      skip_controllers :authorizations, :applications, :authorized_applications
     end
   end
+end
